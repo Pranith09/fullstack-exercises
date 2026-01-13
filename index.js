7adddac2133e014ebbb4be6d947ad2c1e4fd29ca
Pranith09 <pranith.saragadam@gmail.com>
2026-01-13 16:04:16 +0530

    update greeting and add year variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const name = "Student"
-console.log("Hello, " + name + "!")
-console.log("Welcome to Full Stack Open.")
+const name = "Future Developer" // Changed this
+const year = 2026 // Added this
+
+console.log(`Hello, ${name}!`)
+console.log(`It is currently the year ${year}.`)
